e44d7faf874d0df774d4b16cda14960647ee1253
Matt Cave <m@ttcave.ca>
2021-08-12 14:06:11 -0400

    Fix/improve logging in download script

--- download-pages.js
+++ download-pages.js
@@ -12,91 +12,81 @@ let readFileAsync = function (filename) {
 }
 
 
+console.log(`Reading list of homes from list.json`)
 readFileAsync('./list.json', 'UTF-8')
     .then(content => {
         return (JSON.parse(content).result.entries)
     })
     .catch(err => {
-        throw new Error(`Cannot open file = ${err}`)
+        // console.log("No list.json found - downloading a fresh copy")
+        throw new Error(`Cannot open file = ${err.message}`)
     })
     .then(data => {
         // console.log(data)
         return data.map((item) => {
-            return(item.id)
+            return (item.id)
         })
     })
-    .then(data => {
+    .then(idList => {
         // console.log(data)
-        return data.filter(id => {
+        console.log(`Total homes in database: ${idList.length}`)
+        return idList.filter(id => {
             const path = `./pages/${id}.html`
             return !fs.existsSync(path)
         })
-
     })
     .then(filteredIdList => {
-        console.log(`Files to get: ${filteredIdList.length}`)
-        return(filteredIdList)
+        console.log(`Homes still to retrieve: ${filteredIdList.length}`)
+        return (filteredIdList)
     })
     .then(filteredIdList => {
         let delay = 0;
-        for (let i = 0; i < 200 && i < filteredIdList.length; i++) { 
+        for (let i = 0; i < 200 && i < filteredIdList.length; i++) {
             const delayIncrement = 2000;
             // console.log(`Getting ${filteredIdList[i]} with delay ${delay}`)
             setTimeout(getPage, delay, filteredIdList[i])
-            delay += delayIncrement;            
+            delay += delayIncrement;
         }
-        // let promises = [];
-        // const delayIncrement = 1000;
-        // let delay = 0;
-
-        // filteredIdList.forEach(id => {
-        //     console.log(`Requesting ${id}`)
-        //     promises.push(new Promise(resolve => setTimeout(resolve, delay)).then(() =>  
-        //             fetch(`http://www.rhra.ca/en/register/homeid/${id.id}/`, { timeout: 10000})
-        //         )
-        //     );
-        //     // promises.push(fetch(`http://www.rhra.ca/en/register/homeid/${id.id}/`))
-        //     delay += delayIncrement;
-
-        // });
-        // // return promise that is resolved when all images are done loading
-        // return Promise.allSettled(promises);
-
-    })
-
-
-function checkStatus(res) {
-    if (res.ok) { // res.status >= 200 && res.status < 300
-        return res;
-    } else {
-        throw new Error(res.statusText);
-    }
-}
-    
-// getPage("610a816818960")
-
-function getPage(id) {
-    fetch(`http://www.rhra.ca/en/register/homeid/${id}/`, { timeout: 10000})
-    .then(checkStatus)
-    .then(res => res.text())
-    .then(body => {
-        process.stdout.write(".");
-        // console.log(id)
-        // console.log(body)
-        fs.writeFile(`./pages/${id}.html`, body, err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-            
-        })
-        // console.log("File written successfully")
     })
     .catch(err => {
-        process.stdout.write("x");
-        // console.log(`Couldn't get page`)
-        // console.log(err)
+        console.error(err)
     })
 
+function getPage(id) {
+    fetch(`http://www.rhra.ca/en/register/homeid/${id}/`, { timeout: 10000 })
+        .then(checkStatus)
+        .then(res => res.text())
+        .then(body => {
+            process.stdout.write(".");
+            // console.log(id)
+            // console.log(body)
+            fs.writeFile(`./pages/${id}.html`, body, err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+            })
+            // console.log("File written successfully")
+        })
+        .catch(err => {
+            console.err (err)
+            // console.log(`Couldn't get page`)
+        })
+}
 
+function checkStatus(res) {
+    switch (true) {
+        case (res.status >= 200 && res.status < 300):
+            process.stdout.write(".");
+            return res
+        case (res.status >= 400 && res.status < 500):
+            process.stdout.write("4");
+            throw new Error(res.statusText)
+        case (err.status >= 500 && err.status < 600):
+            process.stdout.write("5");
+            throw new Error(res.statusText)
+        default:
+            process.stdout.write("X");
+            throw new Error(res.statusText)
+    }
 }
